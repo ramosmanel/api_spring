6c10736313fb20818ff8b8c60f4c5dfcfff714bb
ramosmanel <emanuelramos20004@gmail.com>
2024-05-10 01:02:44 -0300

    feat: Created GET requisition

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -8,9 +8,12 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -22,5 +25,10 @@ public class ProductController {
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
 }
